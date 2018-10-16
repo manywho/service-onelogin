1cea3b412f0cd662666d087e4ca14183c7fb8964
Jonjo McKay <jonjo.mckay@manywho.com>
2018-10-16 11:41:00 +0100

    Added Servlet 3 support

--- src/main/java/com/boomi/flow/services/onelogin/Application.java
+++ src/main/java/com/boomi/flow/services/onelogin/Application.java
@@ -1,9 +1,19 @@
 package com.boomi.flow.services.onelogin;
 
 import com.manywho.sdk.services.servers.EmbeddedServer;
+import com.manywho.sdk.services.servers.Servlet3Server;
 import com.manywho.sdk.services.servers.undertow.UndertowServer;
 
-public class Application {
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/")
+public class Application extends Servlet3Server {
+    public Application() {
+        this.addModule(new ApplicationModule());
+        this.setApplication(Application.class);
+        this.start();
+    }
+
     public static void main(String[] args) throws Exception {
         EmbeddedServer server = new UndertowServer();
         server.addModule(new ApplicationModule());
